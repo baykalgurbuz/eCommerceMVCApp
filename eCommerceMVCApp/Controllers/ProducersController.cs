2ef0e826b0e8a1468342a971109845509b76cb9f
ebaykalgurbuz <ebaykalgurbuz@gmail.com>
2023-08-06 13:30:07 +0300

    Created Actors View page, passed List data controller to view page

---
+++ eCommerceMVCApp/Controllers/ProducersController.cs
@@ -0,0 +1,22 @@
+﻿using eCommerceMVCApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceMVCApp.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers=await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
